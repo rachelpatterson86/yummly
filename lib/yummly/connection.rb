c5c9d73dfcaa4f4b019c31d69f9e35e350d5c231
Theo Mills <twmills@twmills.com>
2013-05-20 08:48:43 -0500

    Split out URI building into separate methods.

--- lib/yummly/connection.rb
+++ lib/yummly/connection.rb
@@ -1,26 +1,22 @@
 # This class handles the HTTP interactions with the Yummly API calls.
-class Yummly::Connection
+module Yummly
+  class Connection
 
-  attr_accessor :connection
+    attr_accessor :connection
 
-  def self.get(command, params = {})
-    params['_app_id'] = Yummly.configuration.app_id
-    params['_app_key'] = Yummly.configuration.app_key
-    response = self.api_connection.get("/#{self.api_version}/api/#{command}?#{Rack::Utils.build_query(params)}")
-    JSON.parse(response.body)
-  end
-
-  def self.api_connection
-    Faraday.new(:url => "#{self.protocol}://api.yummly.com") do |faraday|
-      faraday.request  :url_encoded             # form-encode POST params
-      faraday.response :logger                  # log requests to STDOUT
-      faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+    def self.get(command, params = {})
+      response = self.api_connection.get(build_uri(command, params))
+      self.parse_response(response)
     end
-  end
 
-  def self.protocol
-    Yummly.configuration.use_ssl? ? 'https' : 'http'
-  end
+    def self.api_connection
+      Faraday.new(:url => "#{self.protocol}://api.yummly.com") do |faraday|
+        faraday.request :url_encoded # form-encode POST params
+        faraday.response :logger # log requests to STDOUT
+        faraday.adapter Faraday.default_adapter # make requests with Net::HTTP
+      end
+    end
+
     def self.parse_response(response)
       case response.status
         when 409 then
@@ -32,8 +28,24 @@ class Yummly::Connection
       end
     end
 
-  def self.api_version
-    Yummly::API_VERSION
-  end
+    def build_uri(command, params)
+      query_string = build_params_query_string(params)
+      "/#{self.api_version}/api/#{command}?#{query_string}"
+    end
 
+    def self.build_params_query_string(params)
+      params['_app_id'] = Yummly.configuration.app_id
+      params['_app_key'] = Yummly.configuration.app_key
+      Rack::Utils.build_query(params)
+    end
+
+    def self.protocol
+      Yummly.configuration.use_ssl? ? 'https' : 'http'
+    end
+
+    def self.api_version
+      Yummly::API_VERSION
+    end
+
+  end
 end
