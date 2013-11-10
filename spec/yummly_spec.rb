6993994828493c4329bf29d8c2cd88c5c23c81ad
Theo Mills <twmills@twmills.com>
2013-11-10 11:26:48 -0600

    Remove inappropriate test.

--- spec/yummly_spec.rb
+++ spec/yummly_spec.rb
@@ -3,8 +3,8 @@ require "spec_helper"
 describe Yummly do
 
   describe ".configure" do
-    let(:app_id) { ENV['YUMMLY_APP_ID'] || "12345" }
-    let(:app_key) { ENV['YUMMLY_APP_KEY'] || "XEARSGSTH12345789" }
+    let(:app_id) { "12345" }
+    let(:app_key) { "XEARSGSTH12345789" }
 
     before do
       Yummly.configure do |config|
@@ -12,13 +12,11 @@ describe Yummly do
         config.app_id = app_id
         config.app_key = app_key
       end
-
     end
 
     specify { Yummly.configuration.use_ssl?.should be_true }
     specify { Yummly.configuration.app_id.should == app_id }
     specify { Yummly.configuration.app_key.should == app_key }
-    specify { Yummly.search("soup recipes").should be_an_instance_of(Array)}
   end
 
 end
