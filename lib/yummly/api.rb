cd6fcbd7dadf5756ef301ccacfe035dcc3ab5ddc
Rachel Patterson <rachel.patterson.1986@gmail.com>
2015-03-18 13:54:37 -0400

    Update Api class to not handle jsonp calls.

--- lib/yummly/api.rb
+++ lib/yummly/api.rb
@@ -34,26 +34,5 @@ module Yummly
       Yummly::SearchResult.new(result)
     end
 
-    #access searchValues of metadata dictionary for excludedIngredient[] &
-    #allowedIngredient[] params for recipe searches.
-    def self.ingredient
-      list = Yummly::Connection.get("metadata/ingredient")
-      Yummly::Ingredient.new(list)
-    end
-
-    #access searchValues of metadata dictionary for allowedAllergy[]
-    #params for recipe searches.
-    def self.allergy
-      list = Yummly::Connection.get("metadata/allergy")
-      Yummly::Allergy.new(list)
-    end
-
-    #access searchValues of metadata dictionary for allowedDiet[]
-    #params for recipe searches.
-    def self.diet
-      list = Yummly::Connection.get("metadata/diet")
-      Yummly::Diet.new(list)
-    end
-
   end
 end
